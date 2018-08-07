ce00f97fcd2812cffe6329c4942cf35b6c56432f
miaowwhappy <183306832@qq.com>
2018-08-07 15:19:17 +0800

    tj

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/blog',function(err){
     	console.log("123");}
     else{
     	console.log("456");
-    	app.listen(6000);
+    	app.listen('127.0.0.1',6000);
     	console.log('server started on port6000');
         }
     }
